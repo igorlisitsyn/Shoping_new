21d2c982e7bb923e2ff69b5cc6655eb4b51cc7ec
igor <truten.vjik@gmail.com>
2022-01-11 20:46:22 +1200

    добавлен новый макет activity_shop_item.xml, и подключение этого макета через class ShopItemActivity, и активация его в манифесте. строка в манифесте android:windowSoftInputMode="adjustResize" - означает, что при вызове клавиатуры, экран автоматически уменьшается.

---
+++ app/src/main/java/com/example/shoping/presentation/ShopItemActivity.kt
@@ -0,0 +1,14 @@
+package com.example.shoping.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoping.R
+
+class ShopItemActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
